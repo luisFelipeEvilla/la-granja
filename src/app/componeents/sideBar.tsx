dc8040c17c566cc6e54f59f5ae749fed50b9af16
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-06-22 15:55:49 -0500

    sidebar

---
+++ src/app/componeents/sideBar.tsx
@@ -0,0 +1,30 @@
+import { MdAddBusiness } from "react-icons/md";
+
+export default function SideBar() {
+
+    const links = [{ label: 'Provedores', href: '/providers', icon: MdAddBusiness }]
+
+    return (
+        <div className="bg-slate-900 w-fit h-screen py-8 text-gray-100">
+            <div className="flex justify-center">
+                <img className="rounded-full h-[80px] w-[80px] " src="/logo.png" alt="logo" />
+            </div>
+            <div className="">
+                <ul>
+                    {
+                        links.map((link, index) =>
+                            <li key={index}>
+                                <a href={link.href} 
+                                    className="flex gap-2 items-center px-3 my-4 text-sm ">
+                                    {<link.icon className="fill-gray-100" />}
+                                    {link.label}
+                                </a>
+                            </li>
+                        )
+                    }
+                </ul>
+            </div>
+        </div>
+
+    )
+}

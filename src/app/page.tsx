48d254a6622ba7f94c50eedb07ce2bd4021eed90
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-07-02 16:02:47 -0500

    filter works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { Card, Table, TableBody, TableCell, TableHead, TableRow, Title, Text, TextInput, Metric, BarChart, Select, SelectItem, DateRangePicker, DateRangePickerValue, MultiSelectItem, MultiSelect } from "@tremor/react";
+import { Card, Table, TableBody, TableCell, TableHead, TableRow, Title, Text, TextInput, Metric, BarChart, Select, SelectItem, DateRangePicker, DateRangePickerValue, MultiSelectItem, MultiSelect, DonutChart } from "@tremor/react";
 import { useEffect, useState } from "react";
 import { Provider } from "@prisma/client";
 import { ProviderWithProducts } from "@/types/Provider";
@@ -16,10 +16,10 @@ export default function Providers() {
 
     useEffect(() => {
         fetch(`/api/providers?startDate=${dates.from?.toISOString()}&endDate=${dates.to?.toISOString()}`)
-        .then(async (res) => {
-            const data = await res.json();
-            setProviders(data);
-        });
+            .then(async (res) => {
+                const data = await res.json();
+                setProviders(data);
+            });
     }, [dates])
 
     useEffect(() => {
@@ -30,7 +30,7 @@ export default function Providers() {
     const updateProducts = (providers: ProviderWithProducts[]) => {
         const aux = providers.map((provider: ProviderWithProducts) => {
             const cantidad = provider.products.reduce((acc, product) => acc + product.quantity, 0);
-            return { provider: `${provider.firstName} ${provider.lastName}`, "Cantidad": cantidad}
+            return { provider: `${provider.firstName} ${provider.lastName}`, Cantidad: cantidad }
         });
 
         setProducts(aux);
@@ -46,7 +46,7 @@ export default function Providers() {
         <div className="grid col-span-1 w-full py-5 gap-4">
             <div className="grid grid-flow-col justify-start gap-3 ml-8" >
                 <DateRangePicker
-                    value={dates} 
+                    value={dates}
                     locale={es}
                     onValueChange={setDates}
                     selectPlaceholder="Fechas"
@@ -60,7 +60,7 @@ export default function Providers() {
                 >
                     {
                         providers.map((provider) => (
-                            <MultiSelectItem   value={provider.id} key={provider.id}>{provider.firstName} {provider.lastName}</MultiSelectItem>
+                            <MultiSelectItem value={provider.id} key={provider.id}>{provider.firstName} {provider.lastName}</MultiSelectItem>
                         ))
                     }
                 </MultiSelect>
@@ -68,7 +68,7 @@ export default function Providers() {
             <div className="flex justify-end mx-32">
                 <Card className="mr-4 w-fit" decoration="top" decorationColor="green">
                     <Text>Proveedores activos</Text>
-                    <Metric>{providers.length}</Metric>
+                    <Metric>{filteredProviders.length}</Metric>
                 </Card>
 
                 <Card className="w-fit" decoration="top" decorationColor="green">
@@ -78,17 +78,32 @@ export default function Providers() {
                     }</Metric>
                 </Card>
             </div>
-            
-            <section className="flex justify-center"> 
-                <Card className="max-w-[800px]">
+
+            <section className="flex justify-center ">
+                <div className="grid gap-4">
                     <Title>Litros de Leche por proveedor</Title>
-                    <BarChart
-                        data={products}
-                        index="provider"
-                        categories={["Cantidad"]}
-                        colors={["blue"]}
-                    />
-                </Card>
+                    <div  className="w-[800px] grid grid-flow-col gap-4">
+                        <Card className="">
+                            <BarChart
+                                className="w-[600px]"
+                                data={products}
+                                index="provider"
+                                categories={["Cantidad"]}
+                                colors={["blue"]}
+                            />
+                        </Card>
+
+                        <Card className="flex items-center w-[200px]" >
+                            <DonutChart
+                                data={products}
+                                index="provider"
+                                category={"Cantidad"}
+                                colors={["blue", "green", "red", "yellow", "purple", "pink", "orange", "indigo", "teal", "cyan"]}
+                            />
+                        </Card>
+                    </div>
+
+                </div>
             </section>
         </div>
     )

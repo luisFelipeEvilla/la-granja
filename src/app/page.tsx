54ab3fa09064ae706a7399dbebfabafd92fda96a
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-07-01 20:04:30 -0500

    date and provider select on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 "use client"
-import { Card, Table, TableBody, TableCell, TableHead, TableRow, Title, Text, TextInput, Metric, BarChart } from "@tremor/react";
+import { Card, Table, TableBody, TableCell, TableHead, TableRow, Title, Text, TextInput, Metric, BarChart, Select, SelectItem, DateRangePicker, DateRangePickerValue } from "@tremor/react";
 import { useEffect, useState } from "react";
 import { Provider } from "@prisma/client";
 import { ProviderWithProducts } from "@/types/Provider";
+import { es } from "date-fns/locale";
 
 export default function Providers() {
     const [search, setSearch] = useState<string>('');
     const [providers, setProviders] = useState<Provider[]>([]);
     const [filteredProviders, setFilteredProviders] = useState<Provider[]>([]);
     const [products, setProducts] = useState<any[]>([]);
+    const [dates, setDates] = useState<DateRangePickerValue>({
+        from: new Date(), to: new Date()
+    });
 
     useEffect(() => {
         fetch('/api/providers')
@@ -32,6 +36,25 @@ export default function Providers() {
 
     return (
         <div className="grid col-span-1 w-full py-5 gap-4">
+            <div className="grid grid-flow-col justify-start gap-3 ml-8" >
+                <DateRangePicker
+                    value={dates} 
+                    locale={es}
+                    onValueChange={setDates}
+                    selectPlaceholder="Fechas"
+                    className="max-w-[300px]"
+                />
+                <Select
+                    placeholder="Seleccionar proveedor"
+                    className="max-w-[300px]"
+                >
+                    {
+                        providers.map((provider) => (
+                            <SelectItem value={provider.id}>{provider.firstName} {provider.lastName}</SelectItem>
+                        ))
+                    }
+                </Select>
+            </div>
             <div className="flex justify-end mx-32">
                 <Card className="mr-4 w-fit" decoration="top" decorationColor="green">
                     <Text>Proveedores activos</Text>
